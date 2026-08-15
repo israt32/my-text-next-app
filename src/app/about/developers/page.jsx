62569baaebc0bdd2830f3d140bbf484b67d5ed83
Israt Jahan <israt5227@gmail.com>
2026-08-15 23:45:46 +0600

    first commit

---
+++ src/app/about/developers/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DevelopersPage = () => {
+  return (
+    <div>
+      <h2 className='text-4xl'>this is developers page</h2>
+    </div>
+  );
+};
+
+export default DevelopersPage;
