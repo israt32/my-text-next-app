3e7a8821ee6221316867d44a2c648c4ad57a377e
Israt Jahan <israt5227@gmail.com>
2026-08-25 15:09:07 +0600

    image optimization

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
+import Image from 'next/image';
 import React from 'react';
 
 const AboutPage = () => {
   return (
     <div>
       <h2>this is about page</h2>
+      <Image src='/don.png' alt='don pic' width='500' height='300'></Image>
     </div>
   );
 };
