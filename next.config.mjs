38e858591d4b509f7bc1029fc6611f6671812700
Israt Jahan <israt5227@gmail.com>
2026-08-25 20:12:30 +0600

    adding meta data

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  // https://plus.unsplash.com/premium_photo-1787073568269-b308af3135f1
   reactCompiler: true,
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import React from 'react';
+import john from '../../assets/images/john.png' 
 
 const AboutPage = () => {
   return (
     <div>
       <h2>this is about page</h2>
+      <Image width="500" height='300' src='https://plus.unsplash.com/premium_photo-1787073568269-b308af3135f1' alt='plant pic'></Image>
       <Image src='/don.png' alt='don pic' width='500' height='300'></Image>
+      <Image width='500' height='300' src={john} alt='cena vai'></Image>
     </div>
   );
 };
